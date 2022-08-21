5da62c2e524f67104950c3bc847e6489a06171fe
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-08-21 15:04:19 +0300

    toolkit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { actions } from "./store/index";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const distpatch = useDispatch();
   const increment = () => {
-    distpatch({ type: "INC" });
+    distpatch(actions.increment());
   };
   const decrement = () => {
-    distpatch({ type: "DEC" });
+    distpatch(actions.decrement());
   };
+  const addBy = () => {
+    distpatch(actions.addBy(10));
+  };
+
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add By 10</button>
     </div>
   );
 }
